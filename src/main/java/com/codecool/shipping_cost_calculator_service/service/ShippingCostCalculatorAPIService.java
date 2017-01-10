a4ed54786e2b55af320d2e613c1c78cbda8c4462
Shevah92 <shevah92@gmail.com>
2017-01-10 13:46:00 +0100

    add google api stuff

--- src/main/java/com/codecool/shipping_cost_calculator_service/service/ShippingCostCalculatorAPIService.java
+++ src/main/java/com/codecool/shipping_cost_calculator_service/service/ShippingCostCalculatorAPIService.java
@@ -4,7 +4,8 @@ package com.codecool.shipping_cost_calculator_service.service;
  * Created by shevah on 10/01/17.
  */
 public class ShippingCostCalculatorAPIService {
-    private static final String URL= "ide jon a link";
+    private static final String GoogleAPIURL= "https://maps.googleapis.com/maps/api/distancematrix/json?";
+    private static final String GoogleAPIKEy = "AIzaSyBLun5iVfffgdRpSLtqmbsyYMrBRYfpG78";
     private static ShippingCostCalculatorAPIService INSTANCE;
 
     public static ShippingCostCalculatorAPIService getINSTANCE() {
