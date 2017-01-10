44fedac37ea81b2c1114d1db9c454c327b8e980b
berloc <berloc1987@gmail.com>
2017-01-10 10:04:22 +0100

    basic project structure

---
+++ src/main/java/com/codecool/nameday_generator_service/NamedayGeneratorService.java
@@ -0,0 +1,7 @@
+package com.codecool.nameday_generator_service;
+
+/**
+ * Created by berloc on 2017.01.10..
+ */
+public class NamedayGeneratorService {
+}
---
+++ src/main/java/com/codecool/shipping_cost_calculator_service/ShippingCostCalculatorService.java
@@ -0,0 +1,7 @@
+package com.codecool.shipping_cost_calculator_service;
+
+/**
+ * Created by berloc on 2017.01.10..
+ */
+public class ShippingCostCalculatorService {
+}
