a5167325e11574d18d890aefddb9ad63b1180c77
Shevah92 <shevah92@gmail.com>
2017-01-10 14:33:54 +0100

    changing https to http in google api url

--- src/main/java/com/codecool/shipping_cost_calculator_service/service/GoogleMapsAPIService.java
+++ src/main/java/com/codecool/shipping_cost_calculator_service/service/GoogleMapsAPIService.java
@@ -4,7 +4,7 @@ package com.codecool.shipping_cost_calculator_service.service;
  * Created by shevah on 10/01/17.
  */
 public class GoogleMapsAPIService {
-    private static final String GoogleAPIURL= "https://maps.googleapis.com/maps/api/distancematrix/json?";
+    private static final String GoogleAPIURL= "http://maps.googleapis.com/maps/api/distancematrix/json?";
     private static final String GoogleAPIKEy = "AIzaSyBLun5iVfffgdRpSLtqmbsyYMrBRYfpG78";
     private static GoogleMapsAPIService INSTANCE;
 
