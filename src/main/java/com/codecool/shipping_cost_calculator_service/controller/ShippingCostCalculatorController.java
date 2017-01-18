c3faace19088917a88efdf7b988609cdbca97411
berloc <berloc1987@gmail.com>
2017-01-18 15:34:58 +0100

    moved extractData & generateErrorJson method from ShippingCostCalculatorController to GoogleMapsAPIService

--- src/main/java/com/codecool/shipping_cost_calculator_service/controller/ShippingCostCalculatorController.java
+++ src/main/java/com/codecool/shipping_cost_calculator_service/controller/ShippingCostCalculatorController.java
@@ -8,7 +8,6 @@ import spark.Response;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.HashMap;
 
 /**
  * Created by shevah on 10/01/17.
@@ -30,11 +29,11 @@ public class ShippingCostCalculatorController {
         String destinationAddress = request.queryParams("destination");
 
         if (originAddress.trim().isEmpty() || destinationAddress.trim().isEmpty()) {
-            return generateErrorJson(response, 400, "Origin or destination cannot be empty or whitespaces only");
+            return apiService.generateErrorJson(response, 400, "Origin or destination cannot be empty or whitespaces only");
         }
 
         String rawData = apiService.requestData(originAddress, destinationAddress);
-        JSONObject extractedData = extractData(response, rawData);
+        JSONObject extractedData = apiService.extractData(response, rawData);
 
         if (extractedData.has("error")) {
             return extractedData;
@@ -49,58 +48,5 @@ public class ShippingCostCalculatorController {
         return allOptionsJSON;
     }
 
-    public JSONObject extractData(Response res, String rawData) {
-        JSONObject rawDataJSON = new JSONObject(rawData);
-        String status = rawDataJSON.getString("status");
-        JSONObject elements;
-        String elementsStatus;
 
-        switch (status){
-            case "OVER_QUERY_LIMIT":
-                return generateErrorJson(res, 429,
-                        "The application has requested too many elements within the allowed time period." +
-                                "The request should succeed if you try again after a reasonable amount of time.");
-            case "REQUEST_DENIED":
-                return generateErrorJson(res, 500,
-                        "The service denied use of the Distance Matrix service by your web page.");
-            case "UNKNOWN_ERROR":
-                return generateErrorJson(res, 500,
-                        "A Distance Matrix request could not be processed due to a server error." +
-                                "The request may succeed if you try again.");
-            case "OK":
-                elements = rawDataJSON.getJSONArray("rows").getJSONObject(0).getJSONArray("elements").getJSONObject(0);
-                elementsStatus = elements.getString("status");
-                break;
-            default:
-                return generateErrorJson(res, 501, "Distance Matrix status message not implemented");
-        }
-
-        switch (elementsStatus) {
-            case "NOT_FOUND":
-                return generateErrorJson(res, 400,
-                        "The origin and/or destination of this pairing could not be geocoded.");
-            case "ZERO_RESULTS":
-                return generateErrorJson(res, 400, "No route could be found between the origin and destination.");
-            case "OK":
-                Integer rawDistance = elements.getJSONObject("distance").getInt("value");
-                Integer rawTime = elements.getJSONObject("duration").getInt("value");
-                HashMap<String, Object> optionValues = new HashMap<>();
-                optionValues.put("dist", rawDistance/1000.f);
-                optionValues.put("time", rawTime/3600.f);
-                optionValues.put("originFound", rawDataJSON.getJSONArray("origin_addresses").get(0));
-                optionValues.put("destinationFound", rawDataJSON.getJSONArray("destination_addresses").get(0));
-                return new JSONObject(optionValues);
-            default:
-                return generateErrorJson(res, 501, "Distance Matrix elements status message not implemented");
-        }
-    }
-
-
-    private JSONObject generateErrorJson(Response response, int statusCode, String message) {
-        response.status(statusCode);
-        response.type("application/json");
-        JSONObject errorJson = new JSONObject();
-        errorJson.put("error", message);
-        return errorJson;
-    }
 }
--- src/main/java/com/codecool/shipping_cost_calculator_service/service/GoogleMapsAPIService.java
+++ src/main/java/com/codecool/shipping_cost_calculator_service/service/GoogleMapsAPIService.java
@@ -2,9 +2,13 @@ package com.codecool.shipping_cost_calculator_service.service;
 
 import org.apache.http.client.fluent.Request;
 import org.apache.http.client.utils.URIBuilder;
+import org.json.JSONObject;
+import spark.Response;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.HashMap;
 
 /**
  * Created by shevah on 10/01/17.
@@ -33,6 +37,61 @@ public class GoogleMapsAPIService {
                 .execute()
                 .returnContent()
                 .asString();
+    }
+
+    public JSONObject extractData(Response res, String rawData) {
+        JSONObject rawDataJSON = new JSONObject(rawData);
+        String status = rawDataJSON.getString("status");
+        JSONObject elements;
+        String elementsStatus;
+
+        switch (status){
+            case "OVER_QUERY_LIMIT":
+                return generateErrorJson(res, 429,
+                        "The application has requested too many elements within the allowed time period." +
+                                "The request should succeed if you try again after a reasonable amount of time.");
+            case "REQUEST_DENIED":
+                return generateErrorJson(res, 500,
+                        "The service denied use of the Distance Matrix service by your web page.");
+            case "UNKNOWN_ERROR":
+                return generateErrorJson(res, 500,
+                        "A Distance Matrix request could not be processed due to a server error." +
+                                "The request may succeed if you try again.");
+            case "OK":
+                elements = rawDataJSON.getJSONArray("rows").getJSONObject(0).getJSONArray("elements").getJSONObject(0);
+                elementsStatus = elements.getString("status");
+                break;
+            default:
+                return generateErrorJson(res, 501, "Distance Matrix status message not implemented");
+        }
+
+        switch (elementsStatus) {
+            case "NOT_FOUND":
+                return generateErrorJson(res, 400,
+                        "The origin and/or destination of this pairing could not be geocoded.");
+            case "ZERO_RESULTS":
+                return generateErrorJson(res, 400, "No route could be found between the origin and destination.");
+            case "OK":
+                Integer rawDistance = elements.getJSONObject("distance").getInt("value");
+                Integer rawTime = elements.getJSONObject("duration").getInt("value");
+                HashMap<String, Object> optionValues = new HashMap<>();
+                optionValues.put("dist", rawDistance/1000.f);
+                optionValues.put("time", rawTime/3600.f);
+                optionValues.put("originFound", rawDataJSON.getJSONArray("origin_addresses").get(0));
+                optionValues.put("destinationFound", rawDataJSON.getJSONArray("destination_addresses").get(0));
+                return new JSONObject(optionValues);
+            default:
+                return generateErrorJson(res, 501, "Distance Matrix elements status message not implemented");
+        }
+    }
+
+
+    public JSONObject generateErrorJson(Response response, int statusCode, String message) {
+        response.status(statusCode);
+        response.type("application/json");
+        JSONObject errorJson = new JSONObject();
+        errorJson.put("error", message);
+        return errorJson;
     }
 
 }
